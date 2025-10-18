b99f25b97f02504686f7d955d9ec93f34a2f8faa
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-18 18:59:07 -0400

    data object tranfer de las respuetas de autenticacion y usuarios

---
+++ src-tauri/src/server/presentation/dtos/responses/auth_responses.rs
@@ -0,0 +1,52 @@
+use serde::Serialize;
+use crate::server::domain::entities::user::User;
+
+#[derive(Debug, Serialize)]
+pub struct AuthResponse {
+    pub access_token: String,
+}
+#[derive(Debug, Serialize)]
+pub struct RegisterResponse {
+    pub status_code: u16,
+    pub message: String,
+    pub error: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct UserResponse {
+    pub id: i32,
+    pub name: String,
+    pub phone: String,
+    pub role: String,
+}
+
+impl From<User> for UserResponse {
+    fn from(user: User) -> Self {
+        Self {
+            id: user.id,
+            name: user.name,
+            phone: user.phone,
+            role: user.role,
+        }
+    }
+}
+
+// Implementación para crear AuthResponse desde User y token
+impl AuthResponse {
+    pub fn from_user_and_token( token: String) -> Self {
+        Self {
+            access_token: token,
+        }
+    }
+}
+
+
+impl RegisterResponse {
+    pub fn success() -> Self {
+        Self {
+            status_code: 201,
+            message: "Usuario registrado exitosamente".to_string(),
+            error: "created".to_string(),
+        }
+    }
+}
