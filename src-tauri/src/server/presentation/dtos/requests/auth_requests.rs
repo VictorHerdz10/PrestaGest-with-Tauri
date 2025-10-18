3f9c1e43a0cbb11324bc3c730aa556fcfcd1e56c
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-18 18:58:49 -0400

    data object tranfer de lass peticiones de autenticacion y usuarios

---
+++ src-tauri/src/server/presentation/dtos/requests/auth_requests.rs
@@ -0,0 +1,23 @@
+use serde::Deserialize;
+use validator::Validate;
+
+#[derive(Debug, Deserialize, Validate)]
+pub struct RegisterRequest {
+    #[validate(length(min = 1, message = "El nombre no puede estar vacío"))]
+    pub name: String,
+    
+    #[validate(length(min = 8, message = "El teléfono debe tener mínimo 8 dígitos"))]
+    pub phone: String,
+    
+    #[validate(length(min = 8, message = "La contraseña debe tener mínimo 8 caracteres"))]
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize, Validate)]
+pub struct LoginRequest {
+    #[validate(length(min = 1, message = "El campo de teléfono no puede estar vacío"))]
+    pub phone: String,
+    
+    #[validate(length(min = 1, message = "El campo de la contraseña no puede estar vacío"))]
+    pub password: String,
+}
