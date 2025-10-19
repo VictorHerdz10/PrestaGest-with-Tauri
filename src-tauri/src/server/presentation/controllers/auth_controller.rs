3697cd06297097c94b6876ff4bb037be7f5912e1
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-18 21:13:47 -0400

    correccionel el response de registra usario, devolviendo statuscode::created

--- src-tauri/src/server/presentation/controllers/auth_controller.rs
+++ src-tauri/src/server/presentation/controllers/auth_controller.rs
@@ -1,7 +1,6 @@
 use crate::get_global_app_state;
 use crate::server::application::use_cases::user_use_cases::UserUseCases;
 use crate::server::infrastructure::database::repositories::user_repository::DieselUserRepository;
-use crate::server::presentation::dtos::responses::auth_responses::RegisterResponse;
 use crate::server::presentation::{
     dtos::{
         requests::auth_requests::{LoginRequest, RegisterRequest},
@@ -10,13 +9,14 @@ use crate::server::presentation::{
     middleware::jwt_middleware::UserPayload,
 };
 use crate::utils::error::{AppError, Result};
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::{extract::Request, Json};
+use serde_json::json;
 use validator::Validate;
 
-pub async fn register(Json(payload): Json<RegisterRequest>) -> Result<Json<RegisterResponse>> {
-    payload
-        .validate()
-        .map_err(|e| AppError::ValidationError(e.to_string()))?;
+pub async fn register(Json(payload): Json<RegisterRequest>) -> Result<impl IntoResponse> {
+    payload.validate().map_err(AppError::from)?;
 
     let app_state = get_global_app_state();
     let user_repository = DieselUserRepository::new(app_state.db.clone());
@@ -26,24 +26,27 @@ pub async fn register(Json(payload): Json<RegisterRequest>) -> Result<Json<Regis
         .register_user(payload.phone, payload.name, payload.password)
         .await?;
 
-    Ok(Json(RegisterResponse::success()))
+    
+    let response = json!({
+        "statusCode": 201,
+        "message": "Usuario registrado exitosamente",
+        "error": "Created"
+    });
+    
+    Ok((StatusCode::CREATED, Json(response)))
 }
 
 pub async fn login(Json(payload): Json<LoginRequest>) -> Result<Json<AuthResponse>> {
-    payload
-        .validate()
-        .map_err(|e| AppError::ValidationError(e.to_string()))?;
+    payload.validate().map_err(AppError::from)?;
 
     let app_state = get_global_app_state();
     let user_repository = DieselUserRepository::new(app_state.db.clone());
     let use_cases = UserUseCases::new(user_repository);
 
-    // Ahora recibimos (User, token) del use case
     let (_, token) = use_cases
         .login_user(payload.phone, payload.password)
         .await?;
 
-    // Construir la respuesta en la capa de presentación
     let auth_response = AuthResponse::from_user_and_token(token);
 
     Ok(Json(auth_response))
@@ -55,7 +58,6 @@ pub async fn get_authenticated_user(request: Request) -> Result<Json<UserRespons
         .get::<UserPayload>()
         .ok_or_else(|| AppError::AuthError("Usuario no autorizado".to_string()))?;
 
-    // Verificar si es admin (similar a tu lógica en NestJS)
     if user_payload.role != "admin" {
         let app_state = get_global_app_state();
         let user_repository = DieselUserRepository::new(app_state.db.clone());
