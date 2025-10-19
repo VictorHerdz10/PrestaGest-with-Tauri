46b4271c495ab07fbd466acae8c97623fbc75fe4
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-19 04:33:26 -0400

    utilizacion del rate a nivel de ruta especifica en auth con limiter strict

--- src-tauri/src/server/presentation/routes/auth_routes.rs
+++ src-tauri/src/server/presentation/routes/auth_routes.rs
@@ -1,13 +1,15 @@
 use axum::{
-    routing::{post, get},
-    Router,
+    middleware, routing::{get, post}, Router
 };
 use crate::server::presentation::controllers::auth_controller::{register, login, get_authenticated_user};
+use crate::server::presentation::middleware::rate_limit::{auth_rate_limit};
+
 
 /// Configura las rutas de autenticación
 pub fn auth_routes() -> Router {
     Router::new()
         .route("/register", post(register))
         .route("/login", post(login))
-        .route("/authenticate", get(get_authenticated_user)) // ✅ Nuevo endpoint protegido
+        .route("/authenticate", get(get_authenticated_user))
+        .layer(middleware::from_fn(auth_rate_limit))
 }
