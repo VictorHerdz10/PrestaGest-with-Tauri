22393e0cb2b8ea6981a46a5c4f50e6defb90c039
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-19 04:32:53 -0400

    utilizacion del rate a nivel de ruta espesifica en user

--- src-tauri/src/server/presentation/routes/user_routes.rs
+++ src-tauri/src/server/presentation/routes/user_routes.rs
@@ -1,12 +1,14 @@
-use axum::{
-    routing::get,
-    Router,
+use crate::server::presentation::controllers::user_controller::{
+    get_user_by_id, get_user_by_phone,
 };
-use crate::server::presentation::controllers::user_controller::{get_user_by_id,get_user_by_phone};
+use crate::server::presentation::middleware::rate_limit::normal_rate_limit;
+use axum::middleware;
+use axum::{routing::get, Router};
 
 /// Configura las rutas de usuarios
 pub fn user_routes() -> Router {
     Router::new()
         .route("/:id", get(get_user_by_id))
         .route("/phone/:phone", get(get_user_by_phone))
+        .layer(middleware::from_fn(normal_rate_limit))
 }
