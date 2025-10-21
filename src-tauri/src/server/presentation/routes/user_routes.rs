46d147592327c3bd480b9ec732672449c5f8ab41
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-21 00:19:24 -0400

    se implemento la inyeccion de dependencias por state en las rutas de usuario

--- src-tauri/src/server/presentation/routes/user_routes.rs
+++ src-tauri/src/server/presentation/routes/user_routes.rs
@@ -1,14 +1,23 @@
+use axum::{
+    middleware,
+    routing::get, 
+    Router
+};
+use crate::server::application::use_cases::user_use_cases::UserUseCases;
+use crate::server::domain::repositories::user_repository::UserRepository;
 use crate::server::presentation::controllers::user_controller::{
     get_user_by_id, get_user_by_phone,
 };
 use crate::server::presentation::middleware::rate_limit::normal_rate_limit;
-use axum::middleware;
-use axum::{routing::get, Router};
 
-/// Configura las rutas de usuarios
-pub fn user_routes() -> Router {
+pub fn user_routes<T>(user_use_cases: UserUseCases<T>) -> Router 
+where 
+    T: UserRepository + Clone + Send + Sync + 'static,
+    UserUseCases<T>: Clone + Send + Sync + 'static,
+{
     Router::new()
         .route("/:id", get(get_user_by_id))
         .route("/phone/:phone", get(get_user_by_phone))
         .layer(middleware::from_fn(normal_rate_limit))
+        .with_state(user_use_cases) // Add state here
 }
