4fbe94ef66a5ceccda22fc2bddf4dc089dcd47f1
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-21 00:29:03 -0400

    implementacion de trait necesariospara axum y injecion de dependencias a las rutas

--- src-tauri/src/server/presentation/routes/api_routes.rs
+++ src-tauri/src/server/presentation/routes/api_routes.rs
@@ -1,11 +1,18 @@
-// api_routes.rs
-use crate::server::presentation::middleware::jwt_middleware;
-use crate::server::presentation::routes::{auth_routes, user_routes};
 use axum::{middleware, Router};
+use crate::server::presentation::routes::{auth_routes::auth_routes, user_routes::user_routes};
+use crate::server::presentation::middleware::jwt_middleware::jwt_middleware;
+use crate::server::application::use_cases::user_use_cases::UserUseCases;
+use crate::server::domain::repositories::user_repository::UserRepository;
 
-pub fn api_routes() -> Router {
+pub fn api_routes<T>(user_use_cases: UserUseCases<T>) -> Router 
+where 
+    T: UserRepository + Clone + Send + Sync + 'static,
+{
     Router::new()
-        .nest("/auth", auth_routes::auth_routes())
-        .nest("/users", user_routes::user_routes())
-        .layer(middleware::from_fn(jwt_middleware::jwt_middleware))
+        .nest("/auth", auth_routes(user_use_cases.clone()))
+        .nest("/users", user_routes(user_use_cases.clone()))
+        .layer(middleware::from_fn_with_state(
+            user_use_cases, 
+            jwt_middleware
+        ))
 }
