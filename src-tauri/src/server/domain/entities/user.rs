3d4a08b577705f53c5fd820c4f9199999c2693b0
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-21 00:24:14 -0400

    add el payload de uario para mejor separacion dee reponsabilidades y estructura limpia

--- src-tauri/src/server/domain/entities/user.rs
+++ src-tauri/src/server/domain/entities/user.rs
@@ -1,21 +1,33 @@
-use serde::{Deserialize, Serialize};
-use chrono::{DateTime, Utc};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct User {
     pub id: i32,
     pub phone: String,
     pub name: String,
     pub password: String,
     pub role: String,
-    pub created_at: DateTime<Utc>,
-    pub updated_at: DateTime<Utc>,
+    //pub created_at: String,
+    //pub updated_at: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct NewUser {
     pub phone: String,
     pub name: String,
     pub password: String,
     pub role: String,
+}
+
+#[derive(Clone)]
+pub struct UserPayload {
+    pub id: i32,
+    pub name: String,
+    pub phone: String,
+    pub role: String,
+}
+
+impl UserPayload {
+    pub fn new(id: i32, name: String, phone: String, role: String) -> Self {
+        Self { id, name, phone, role }
+    }
 }
