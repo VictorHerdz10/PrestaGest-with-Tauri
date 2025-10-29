6e928538329bf4873afc24820cd67132c104efc1
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-29 04:04:05 -0400

    add fn para crear pool de borrower en una sola intacia para inyectarla a las rutas

--- src-tauri/src/utils/service_factory.rs
+++ src-tauri/src/utils/service_factory.rs
@@ -1,4 +1,6 @@
 use crate::get_global_app_state;
+use crate::server::application::use_cases::borrower_use_cases::BorrowerUseCases;
+use crate::server::infrastructure::database::repositories::diesel_borrower_repository::DieselBorrowerRepository;
 use crate::server::infrastructure::database::repositories::diesel_user_repository::DieselUserRepository;
 use crate::server::application::use_cases::user_use_cases::UserUseCases;
 
@@ -15,6 +17,13 @@ impl ServiceFactory {
             app_state.config.server.jwt_secret.clone(),
             app_state.config.app.master_password.clone()
         )
+    }
+    pub fn create_borrower_use_cases() -> BorrowerUseCases<DieselBorrowerRepository> {
+        let app_state = get_global_app_state();
+        
+        let borrower_repository = DieselBorrowerRepository::new(app_state.db.clone());
+        
+        BorrowerUseCases::new(borrower_repository)
     }
     
 }
