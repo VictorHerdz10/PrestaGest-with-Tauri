59711064bf595236e825df26e701da294a41e5ba
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-29 04:04:46 -0400

    agregado parametors genericos apralos nuevos use cases inyectados

--- src-tauri/src/server/presentation/routes/api_routes.rs
+++ src-tauri/src/server/presentation/routes/api_routes.rs
@@ -1,16 +1,28 @@
 use axum::{middleware, Router};
-use crate::server::presentation::routes::{auth_routes::auth_routes, user_routes::user_routes};
+use crate::server::presentation::routes::{
+    auth_routes::auth_routes, 
+    user_routes::user_routes, 
+    borrower_routes::borrower_routes
+};
 use crate::server::presentation::middleware::jwt_middleware::jwt_middleware;
-use crate::server::application::use_cases::user_use_cases::UserUseCases;
-use crate::server::domain::repositories::user_repository::UserRepository;
+use crate::server::application::use_cases::{
+    user_use_cases::UserUseCases,
+    borrower_use_cases::BorrowerUseCases  // ← Añadir
+};
+use crate::server::domain::repositories::{
+    user_repository::UserRepository,
+    borrower_repository::BorrowerRepository  // ← Añadir
+};
 
-pub fn api_routes<T>(user_use_cases: UserUseCases<T>) -> Router 
+pub fn api_routes<T, U>(user_use_cases: UserUseCases<T>, borrower_use_cases: BorrowerUseCases<U>) -> Router 
 where 
     T: UserRepository + Clone + Send + Sync + 'static,
+    U: BorrowerRepository + Clone + Send + Sync + 'static,
 {
     Router::new()
         .nest("/auth", auth_routes(user_use_cases.clone()))
         .nest("/users", user_routes(user_use_cases.clone()))
+        .nest("/borrowers", borrower_routes(borrower_use_cases)) 
         .layer(middleware::from_fn_with_state(
             user_use_cases, 
             jwt_middleware
